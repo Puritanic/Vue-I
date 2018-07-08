203d9fa52f8ff7c23cf38d35659f9b8891061b08
Darko Tasevski <darktasevski@gmail.com>
2018-07-08 17:15:48 +0200

    My hands are typing code

--- main.js
+++ main.js
@@ -4,12 +4,61 @@ new Vue({
 		title: 'Hello world',
 		link: 'https://google.com',
 	},
+	computed: {
+		finishedLink() {
+			return `<a href=${this.link}> Google html a</a>`;
+		},
+	},
 	methods: {
 		changeTitle(e) {
 			this.title = e.target.value;
 		},
 		sayHello() {
-			return 'Hello to ';
+			this.title = 'Hello';
+			return 'Say Hello To ';
 		},
 	},
+});
+
+new Vue({
+	el: '#counter',
+	data: {
+		counter: 0,
+		x: 0,
+		y: 0,
+	},
+	methods: {
+		incrementCounter() {
+			return this.counter++;
+		},
+		decrementCounter() {
+			return this.counter--;
+		},
+		updateCoords(e) {
+			(this.x = e.clientX), (this.y = e.clientY);
+		},
+		stepUp(step, event) {
+			return (this.counter += step);
+		},
+		dummy(e) {
+			e.stopPropagation();
+		},
+		alertMe() {
+			alert('Alert!');
+		},
+	},
+});
+
+new Vue({
+	el: '#model',
+	data: {
+		name: 'Darko',
+		counter: 0,
+	},
+	computed: {
+		output() {
+			return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+		},
+	},
+	methods: {},
 });
